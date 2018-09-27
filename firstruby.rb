1948239707d185c4d5491da2fa4f914172406316
Taniya Amidon <harshanitaniya@yahoo.co.uk>
2018-09-27 10:15:28 +0100

    more activities

--- firstruby.rb
+++ firstruby.rb
@@ -1,6 +1,7 @@
-# Learn to program activities 
+# Chris Pine Learn to program book - activities 
 
 # character count in a name 
+
 puts "What is your first name? "
 first_name = gets.chomp
 puts "What is your last name? "
@@ -10,13 +11,16 @@ character = first_name.length + last_name.length
 
 puts "Your name " + first_name + last_name + ", has " + character.to_s + " characters."
 
+
 # string methods 
+
 puts first_name.upcase
 puts last_name.swapcase
 puts first_name.downcase
 
 
 # align content 
+
 line_width = 50
 puts (                  "Twinkle".center(line_width))
 puts (                  "Twinkle".center(line_width))
@@ -28,6 +32,7 @@ name = "Taniya"
 puts (name.ljust( line_width))
 puts (name.center(line_width))
 puts (name.rjust( line_width))
+
 
 # Angry boss program
 
@@ -45,7 +50,9 @@ puts (("Chapeter 1: Getting Started").ljust(line_width)+("page 1").rjust(line_wi
 puts (("Chapeter 2: Number").ljust(line_width) +        ("page 9").rjust(line_width))
 puts (("Chapeter 3: Letters").ljust(line_width) +      ("page 13").rjust(line_width))
 
+
 # comparison method
+
 puts 1 > 3
 puts 2 < 1
 puts 5 >= 5
@@ -53,6 +60,7 @@ puts 5 <= 4
 
 
 # branching
+
 puts "Hello, I'm a fortune teller. Tell me your name: "
 name = gets.chomp
 
@@ -71,7 +79,59 @@ else
 end
 
 
+# Looping
+
+while 1 < 2
+    input = gets.chomp
+    puts input
+    if input == "bye"
+        break 
+    end
+end 
+puts "Come again soon!"
 
 
+puts "Hello what\'s your name?"
+name = gets.chomp
+puts "Hello," + name + "."
+
+if name == "taniya" || name == "gab"
+    puts "What a lovely name!"
+end
 
 
+# Arrays
+
+foods = ["Apple", "Rice", "Chicken"]
+
+puts foods
+puts
+puts foods.to_s
+puts
+puts foods.join(",")
+puts
+puts foods.join(" :) ") + " 8)"
+
+
+favorites = []
+favorites.push "My name is Taniya"
+favorites.push "I'm Australian"
+
+puts favorites[0]
+puts favorites.last
+puts favorites.length
+
+puts favorites.pop
+puts favorites
+puts favorites.length
+
+
+#define
+
+def double_num num 
+    num_times_2 = num*2
+    puts num.to_s + " doubled is " + num_times_2.to_s
+    
+end
+
+double_num 30
